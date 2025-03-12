7c47e6d8369055e2c9d0ca105a8f2a72d56aa4ff
Juan Carlos Lloret Hernández <juan.carlos.lloret@lansweeper.com>
2025-03-12 13:40:15 +0100

    Created benchmark suite and benchmark test abstraction

--- internal/benchmark/explainBenchmarl.go
+++ internal/benchmark/explainBenchmarl.go
@@ -5,35 +5,49 @@ import (
 	"encoding/json"
 	"fmt"
 	"reflect"
+	"strings"
 
 	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/PaesslerAG/gval"
 	"github.com/PaesslerAG/jsonpath"
-	"github.com/lansweeper/ClickhouseBenchTool/internal"
 )
 
-func flatten(nested []interface{}) []interface{} {
-	flattened := make([]interface{}, 0)
-	for _, i := range nested {
-		switch v := i.(type) {
-		case []interface{}:
-			flattenedSubArray := flatten(v)
-			flattened = append(flattened, flattenedSubArray...)
-		case interface{}:
-			flattened = append(flattened, v)
-		default:
-		}
-	}
-	return flattened
+type queryExplainResultData struct {
+	Type     string
+	Keys     []string
+	Parts    [2]float64
+	Granules [2]float64
 }
 
-func ExplainBenchmark(ctx context.Context, conn clickhouse.Conn, query string) (internal.QueryExplainBenchmarkResult, error) {
-	result := internal.QueryExplainBenchmarkResult{}
+type queryExplainBenchmarkData struct {
+	MinMax     queryExplainResultData
+	Partition  queryExplainResultData
+	PrimaryKey queryExplainResultData
+}
+
+const ExplainBenchmarkName = "explainBenchmark"
+
+type ExplainBenchmark struct {
+	conn clickhouse.Conn
+}
+
+func NewExplainBenchmark(conn clickhouse.Conn) *ExplainBenchmark {
+	return &ExplainBenchmark{
+		conn: conn,
+	}
+}
+
+func (qlb *ExplainBenchmark) Name() string {
+	return ExplainBenchmarkName
+}
+
+func (qlb *ExplainBenchmark) Run(ctx context.Context, queryParams map[string]any, query string) (map[string]string, error) {
+	result := queryExplainBenchmarkData{}
 	explainQuery := fmt.Sprintf("EXPLAIN indexes = 1, json = 1, description = 1 %s FORMAT TSVRaw", query)
 
-	explainRow, err := conn.Query(ctx, explainQuery)
+	explainRow, err := qlb.conn.Query(ctx, explainQuery)
 	if err != nil {
-		return result, fmt.Errorf("run explain benchmark query: %w", err)
+		return map[string]string{}, fmt.Errorf("run explain benchmark query: %w", err)
 	}
 
 	// The structure of the explain result is dynamic, depends on each query.
@@ -46,7 +60,7 @@ func ExplainBenchmark(ctx context.Context, conn clickhouse.Conn, query string) (
 	explainRow.Next()
 	err = explainRow.Scan(rawResult...)
 	if err != nil {
-		return result, fmt.Errorf("scan explain query rows: %w", err)
+		return map[string]string{}, fmt.Errorf("scan explain query rows: %w", err)
 	}
 	explainRow.Close() // We now there is only one row
 
@@ -54,7 +68,7 @@ func ExplainBenchmark(ctx context.Context, conn clickhouse.Conn, query string) (
 	resultsData := interface{}(nil)
 	err = json.Unmarshal([]byte(explainResultBytes), &resultsData)
 	if err != nil {
-		return result, fmt.Errorf("unmarshal explain result: %w", err)
+		return map[string]string{}, fmt.Errorf("unmarshal explain result: %w", err)
 	}
 
 	// Because the response is dynamic and complex the library jsonpath and gval are
@@ -62,11 +76,11 @@ func ExplainBenchmark(ctx context.Context, conn clickhouse.Conn, query string) (
 	builder := gval.Full(jsonpath.PlaceholderExtension())
 	path, err := builder.NewEvaluable("$..Indexes")
 	if err != nil {
-		return result, fmt.Errorf("build jsonpath query to analyze explain query results: %w", err)
+		return map[string]string{}, fmt.Errorf("build jsonpath query to analyze explain query results: %w", err)
 	}
 	data, err := path(context.Background(), resultsData)
 	if err != nil {
-		return result, fmt.Errorf("execute jsonpath query to analyze explain query results: %w", err)
+		return map[string]string{}, fmt.Errorf("execute jsonpath query to analyze explain query results: %w", err)
 	}
 
 	// The result is a nested list of maps, so it is necessary to flatten it and then
@@ -80,7 +94,7 @@ func ExplainBenchmark(ctx context.Context, conn clickhouse.Conn, query string) (
 				resultKeys = append(resultKeys, key.(string))
 			}
 		}
-		resultsMap := internal.QueryExplainResultData{
+		resultsMap := queryExplainResultData{
 			Type: elemMap["Type"].(string),
 			Keys: resultKeys,
 			Parts: [2]float64{
@@ -102,5 +116,41 @@ func ExplainBenchmark(ctx context.Context, conn clickhouse.Conn, query string) (
 			result.PrimaryKey = resultsMap
 		}
 	}
-	return result, nil
+	return map[string]string{
+		"minMaxGranules":     joinNumberArray(result.MinMax.Granules[:]),
+		"minMaxParts":        joinNumberArray(result.MinMax.Parts[:]),
+		"minMaxkeys":         strings.Join(result.MinMax.Keys, "/"),
+		"partitionGranules":  joinNumberArray(result.Partition.Granules[:]),
+		"partitionParts":     joinNumberArray(result.Partition.Parts[:]),
+		"partitionkeys":      strings.Join(result.Partition.Keys, "/"),
+		"primaryKeyGranules": joinNumberArray(result.PrimaryKey.Granules[:]),
+		"primaryKeyParts":    joinNumberArray(result.PrimaryKey.Parts[:]),
+		"primaryKeykeys":     strings.Join(result.PrimaryKey.Keys, "/"),
+	}, nil
+}
+
+func flatten(nested []interface{}) []interface{} {
+	flattened := make([]interface{}, 0)
+	for _, i := range nested {
+		switch v := i.(type) {
+		case []interface{}:
+			flattenedSubArray := flatten(v)
+			flattened = append(flattened, flattenedSubArray...)
+		case interface{}:
+			flattened = append(flattened, v)
+		default:
+		}
+	}
+	return flattened
+}
+
+func joinNumberArray(arr []float64) string {
+	var result string
+	for i, v := range arr {
+		result += fmt.Sprintf("%v", v)
+		if i < len(arr)-1 {
+			result += "/"
+		}
+	}
+	return result
 }
--- internal/benchmark/queryLogBenchmark.go
+++ internal/benchmark/queryLogBenchmark.go
@@ -8,12 +8,39 @@ import (
 	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
 	"github.com/google/uuid"
-	"github.com/lansweeper/ClickhouseBenchTool/internal"
 )
 
-func RunQueryLogBenchmark(ctx context.Context, conn clickhouse.Conn, query string, queryParams map[string]any) (internal.QueryLogBenchmarkResults, error) {
+const QueryLogBenchmarkName = "queryLogBenchmark"
+
+type queryLogChResults struct {
+	ReadBytes                uint64 `ch:"read_bytes"`
+	ReadBytesReadable        string `ch:"read_bytes_readable"`
+	ResultBytes              uint64 `ch:"result_bytes"`
+	ResultBytesReadable      string `ch:"result_bytes_readable"`
+	ReadRows                 uint64 `ch:"read_rows"`
+	ResultRows               uint64 `ch:"result_rows"`
+	MemoryUsage              uint64 `ch:"memory_usage"`
+	MemoryUsageReadable      string `ch:"memory_usage_readable"`
+	OSCPUVirtualTime         uint64 `ch:"os_cpu_virtual"`
+	OSCPUVirtualTimeReadable string `ch:"os_cpu_virtual_readable"`
+}
+
+type QueryLogBenchmark struct {
+	conn clickhouse.Conn
+}
+
+func NewQueryLogBenchmark(conn clickhouse.Conn) *QueryLogBenchmark {
+	return &QueryLogBenchmark{
+		conn: conn,
+	}
+}
+
+func (qlb *QueryLogBenchmark) Name() string {
+	return QueryLogBenchmarkName
+}
+
+func (qlb *QueryLogBenchmark) Run(ctx context.Context, queryParams map[string]any, query string) (map[string]string, error) {
 	queryId := uuid.New()
-	queryLogResults := internal.QueryLogBenchmarkResults{}
 
 	params := []driver.NamedValue{}
 	for k, v := range queryParams {
@@ -24,9 +51,9 @@ func RunQueryLogBenchmark(ctx context.Context, conn clickhouse.Conn, query strin
 		interfaceParams[i] = v
 	}
 
-	rows, err := conn.Query(clickhouse.Context(ctx, clickhouse.WithQueryID(queryId.String())), query, interfaceParams...)
+	rows, err := qlb.conn.Query(clickhouse.Context(ctx, clickhouse.WithQueryID(queryId.String())), query, interfaceParams...)
 	if err != nil {
-		return queryLogResults, fmt.Errorf("run query log benchmark: %w", err)
+		return map[string]string{}, fmt.Errorf("run query log benchmark: %w", err)
 	}
 
 	var columnTypes = rows.ColumnTypes()
@@ -37,24 +64,26 @@ func RunQueryLogBenchmark(ctx context.Context, conn clickhouse.Conn, query strin
 		}
 		err := rows.Scan(vars...)
 		if err != nil {
-			return queryLogResults, fmt.Errorf("scan row: %w", err)
+			return map[string]string{}, fmt.Errorf("scan row: %w", err)
 		}
 	}
 	// Search query in query logs
-	err = conn.Exec(ctx, "SYSTEM FLUSH LOGS ON CLUSTER default")
+	err = qlb.conn.Exec(ctx, "SYSTEM FLUSH LOGS ON CLUSTER default")
 	if err != nil {
-		return queryLogResults, fmt.Errorf("flush logs: %w", err)
+		return map[string]string{}, fmt.Errorf("flush logs: %w", err)
 	}
-	row := conn.QueryRow(ctx, `
+	row := qlb.conn.QueryRow(ctx, `
 		SELECT
 			read_bytes,
+			formatReadableSize(read_bytes) as read_bytes_readable,
 			result_bytes,
+			formatReadableSize(result_bytes) as result_bytes_readable,
 			read_rows,
 			result_rows,
 			memory_usage,
-			ProfileEvents [ 'OSCPUVirtualTimeMicroseconds' ] as os_cpu_virtual_time_microseconds,
-			query,
-			query_id
+			formatReadableSize(memory_usage) as memory_usage_readable,
+			ProfileEvents [ 'OSCPUVirtualTimeMicroseconds' ] as os_cpu_virtual,
+			formatReadableTimeDelta(ProfileEvents [ 'OSCPUVirtualTimeMicroseconds' ]/1000000, 'seconds', 'microseconds') as os_cpu_virtual_readable
 		FROM
 			clusterAllReplicas('default', system.query_log)
 		WHERE
@@ -62,10 +91,22 @@ func RunQueryLogBenchmark(ctx context.Context, conn clickhouse.Conn, query strin
 			AND client_name LIKE 'clickhouse-benchmark-tool%%'
 			AND query_id = $1`, queryId.String())
 
+	var queryLogResults queryLogChResults
 	err = row.ScanStruct(&queryLogResults)
 	if err != nil {
-		return queryLogResults, fmt.Errorf("scan query log row: %w", err)
+		return map[string]string{}, fmt.Errorf("scan query log row: %w", err)
 	}
 
-	return queryLogResults, nil
+	return map[string]string{
+		"read_bytes":                   fmt.Sprintf("%v", queryLogResults.ReadBytes),
+		"read_bytes_readable":          fmt.Sprintf("%v", queryLogResults.ReadBytesReadable),
+		"result_bytes":                 fmt.Sprintf("%v", queryLogResults.ResultBytes),
+		"result_bytes_readable":        fmt.Sprintf("%v", queryLogResults.ResultBytesReadable),
+		"read_rows":                    fmt.Sprintf("%v", queryLogResults.ReadRows),
+		"result_rows":                  fmt.Sprintf("%v", queryLogResults.ResultRows),
+		"memory_usage":                 fmt.Sprintf("%v", queryLogResults.MemoryUsage),
+		"memory_usage_readable":        fmt.Sprintf("%v", queryLogResults.MemoryUsageReadable),
+		"os_cpu_virtual_time":          fmt.Sprintf("%v", queryLogResults.OSCPUVirtualTime),
+		"os_cpu_virtual_time_readable": fmt.Sprintf("%v", queryLogResults.OSCPUVirtualTimeReadable),
+	}, nil
 }
